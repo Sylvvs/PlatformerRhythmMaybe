2275d3dfc949c2a6bed769136562ce377d58f86f
Sylvvs <kongsted.simon@gmail.com>
2025-09-24 13:02:44 +0200

    dash no speed decay

--- platformer-rhythm-maybe/Scenes/Player/player.gd
+++ platformer-rhythm-maybe/Scenes/Player/player.gd
@@ -1,23 +1,87 @@
 extends CharacterBody2D
 
-const SPEED_ACC = 20
-const SPEED_MAX = 200
+const SPEED_ACC = 25
+const SPEED_MAX = 100
+const JUMP_FORCE = -175
+const GRAVITY = 7
 
-const JUMP_FORCE = -200
+# Dash
+const DASH_SPEED = 400
+const DASH_TIME = 0.15
 
-func _physics_process(_delta: float) -> void:
-	up_direction = Vector2(0,-1)
-	if Input.is_action_pressed("walk_left"):
-		velocity.x = max(velocity.x - SPEED_ACC, -SPEED_MAX)
-	elif Input.is_action_pressed("walk_right"):
-		velocity.x = min(velocity.x + SPEED_ACC, SPEED_MAX)
-	else:
-		velocity.x = 0
+var dashes := 1
+var dash_timer := DASH_TIME
+var dash_dir := Vector2.ZERO
+
+var last_x_dir := 0
+var last_y_dir := 0
+
+func _physics_process(delta: float) -> void:
+	var input_vec := get_input_vector()
+	
+	if dash_timer >= DASH_TIME:
+		if input_vec.x == -1:
+			if velocity.x > -SPEED_MAX: velocity.x -= SPEED_ACC
+		elif velocity.x < 0: velocity.x += SPEED_ACC
+		if input_vec.x == 1:
+			if velocity.x < SPEED_MAX: velocity.x += SPEED_ACC
+		elif velocity.x > 0: velocity.x -= SPEED_ACC
+
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_FORCE
+		dash_timer = DASH_TIME
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = 0
 
-	if not is_on_floor():
-		velocity.y += 10
+
+	if Input.is_action_just_pressed("dash") and dashes > 0 and dash_timer >= DASH_TIME:
+		dashes -= 1
+		dash_timer = 0.0
+		velocity = Vector2.ZERO
+		dash_dir = input_vec
+		if dash_dir == Vector2.ZERO:
+			if velocity.x != 0:
+				dash_dir = Vector2(sign(velocity.x), 0)
+			else:
+				dash_dir = Vector2.RIGHT
+		dash_dir = dash_dir.normalized()
+		velocity += dash_dir * DASH_SPEED
+
+	if dash_timer < DASH_TIME:
+		dash_timer += delta
+		if dash_timer >= DASH_TIME:
+			velocity = Vector2.ZERO
+			dash_timer = DASH_TIME
+
+	if dash_timer >= DASH_TIME and !is_on_floor():
+		velocity.y += GRAVITY 
+		 
+	if dash_timer >= DASH_TIME and is_on_floor():
+		dashes = 1
 
 	move_and_slide()
+
+func get_input_vector() -> Vector2:
+	last_x_dir = get_axis_dir("walk_left", "walk_right", last_x_dir)
+	last_y_dir = get_axis_dir("up", "down", last_y_dir)
+	return Vector2(last_x_dir, last_y_dir)
+
+func get_axis_dir(neg_action: String, pos_action: String, last_dir: int) -> int:
+	if Input.is_action_just_pressed(neg_action):
+		last_dir = -1
+	elif Input.is_action_just_pressed(pos_action):
+		last_dir = 1
+
+	elif Input.is_action_just_released(neg_action):
+		if Input.is_action_pressed(pos_action):
+			last_dir = 1
+		else:
+			last_dir = 0
+	elif Input.is_action_just_released(pos_action):
+		if Input.is_action_pressed(neg_action):
+			last_dir = -1
+		else:
+			last_dir = 0
+
+	return last_dir
