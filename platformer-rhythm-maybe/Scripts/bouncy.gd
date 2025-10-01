bac835e3a360272b4c22902816e86bc53fce1d9d
Sylvvs <kongsted.simon@gmail.com>
2025-10-01 09:35:38 +0200

    level transition and bouncy logic adjusted

--- platformer-rhythm-maybe/Scripts/bouncy.gd
+++ platformer-rhythm-maybe/Scripts/bouncy.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-
+var power = 350;
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		body.velocity.y = -350
+		body.velocity.y = -power
 		body.dashes = 1;
