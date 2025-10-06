8adddfd352910c7d27bedfeba247cdf635776376
Maffeq <Victor.scharf.heinemann@outlook.dk>
2025-10-06 13:44:00 +0200

    SFX spring og jump

--- platformer-rhythm-maybe/Scripts/bouncy.gd
+++ platformer-rhythm-maybe/Scripts/bouncy.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+@onready var SpringSFX = $SpringSFX
 var power = 350;
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		body.velocity.y = -power
 		body.dashes = 1;
+		SpringSFX.play()
